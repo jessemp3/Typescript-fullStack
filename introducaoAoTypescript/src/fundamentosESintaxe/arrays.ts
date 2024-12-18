8335422f232e0c1e217ba0d69980d221b9de1633
jesse <nitchsx@gmail.com>
2024-12-17 22:00:01 -0300

    feat: class of arrays

---
+++ introducaoAoTypescript/src/fundamentosESintaxe/arrays.ts
@@ -0,0 +1,20 @@
+const array: Array<number> = [1, 2, 3, 4, 5];;//primeira forma de declarar um array
+const array2: string[] = ["1", "2", "3", "4", "5"]; //segunda forma de declarar um array
+const array3 : string[] =  [ "jesse" , "jessica", "jose", "joao"] //terceira forma de declarar um array
+
+
+console.log(array.length); // 5
+// array.push(6); // adiciona um elemento no final do array
+console.log(array.length); // [1, 2, 3, 4, 5, 6]
+
+//bucando um elemento no array
+const busca = array.find(elemento => elemento >2 )
+// console.log(busca); // 3
+
+
+
+array.map(num => {
+    if (num % 2 === 1) {
+        console.log(num);
+    }
+});
