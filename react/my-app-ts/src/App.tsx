2260fb944506c76ea14b642379f20b51bdbdb508
jesse <nitchsx@gmail.com>
2025-01-02 11:38:36 -0300

    feat : class of react

--- react/my-app-ts/src/App.tsx
+++ react/my-app-ts/src/App.tsx
@@ -1,12 +1,25 @@
 import { Card}from "./Components/Card.tsx";
+import {Layout} from "./Components/layout.tsx";
 
 function App() {
 
   return (
-    <>
-        <Card id={1} />
-        <Card id={2} />
-    </>
+     <Layout>
+         aa
+        <Card
+            url='https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=600'
+            id={1}
+            title="Some quick example text to build on the card title and make up the bulk of
+                        the card's content"
+
+        />
+        <Card
+            url='https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=600'
+            id={2}
+            title="Some quick example text to build on the card title and make up the bulk of
+                        the card's content" />
+     </Layout>
+
   )
 }
 
--- react/my-app-ts/src/Components/Card.tsx
+++ react/my-app-ts/src/Components/Card.tsx
@@ -1,16 +1,18 @@
 interface ICard {
-    id:number
+    id:number,
+    title:string,
+    url:string,
+
 }
 
-export const Card = ({id}:ICard) =>{
+export const Card = ({id, title, url}:ICard) =>{
     return (
         <>
             <div className="card">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzPZGDfAQ2gPTdsXl5Qw_rwxKEpLKG5ts9qg&s" className="card-img-top" alt="..."/>
+                <img src={url} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">Card {id}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                        the card's content.</p>
+                    <p className="card-text">{title}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
